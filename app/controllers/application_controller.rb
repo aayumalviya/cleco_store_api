b83da1df5b85dd6bcd88bf0ab12999e648ab3147
aayumalviya <aayushi.malviya@clecotech.com>
2022-11-09 16:33:56 +0530

    Remove a  gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
     # add_flash_types :info, :error, :warning
 
-#   acts_as_token_authentication_handler_for User
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  # acts_as_token_authenticatable
-  # field :authentication_token
   has_many :user_logins, dependent: :destroy
 
   devise :database_authenticatable, :registerable,
