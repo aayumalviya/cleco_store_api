68adf6e9298c8f607d3c418541fe35b5d684455c
manish-shrivastava <er.manishshrivastava@gmail.com>
2022-11-22 17:19:59 +0530

    add validation

--- app/models/delivery_address.rb
+++ app/models/delivery_address.rb
@@ -1,3 +1,7 @@
 class DeliveryAddress < ApplicationRecord
+  validates :address, uniqueness: { scope: [:user_id] }
+  validates :name, :pincode, :locality, :address, :city, :state, :landmark, :alternate_phone, :address_type, presence: true
+  validates :mobile_number, presence: true,
+                    :length => { :minimum => 10, :maximum => 13}
   belongs_to :user
 end
