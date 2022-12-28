cc20c22d52584ddeeeb427bd4ce6049977bf80a5
manish-shrivastava <er.manishshrivastava@gmail.com>
2022-12-28 16:56:40 +0530

    Deployment

---
+++ config/deploy.rb
@@ -0,0 +1,28 @@
+lock "~> 3.16.0"
+
+# replace obvious parts
+server '3.217.135.81', port: 22, roles: [:web, :app, :db], primary: true
+set :application, "clecostore"
+set :repo_url, "git@github.com:aayumalviya/cleco_store_api.git"
+
+set :user, 'deploy'
+set :puma_threads,    [4, 16]
+set :puma_workers,    0
+
+set :pty,             true
+set :use_sudo,        false
+set :stage,           :production
+set :deploy_via,      :remote_cache
+set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.access.log"
+set :puma_error_log,  "#{release_path}/log/puma.error.log"
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+set :puma_preload_app, true
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true  # Change to false when not using ActiveRecord
+
+append :linked_files, "config/master.key"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "public/uploads"
