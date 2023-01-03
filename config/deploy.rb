a6ded4cb3a1df3b0c583d567e3007d7be4fa1c35
manish-shrivastava <er.manishshrivastava@gmail.com>
2023-01-03 12:59:31 +0530

    change deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,162 +1,65 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.17.1"
-# server '3.217.135.81', user: 'deploy', roles: %w{app db web}
+server '3.217.135.81', user: 'deploy', roles: %w{app db web}
 
-# set :application, 'clecostore'
-# set :pty, true
-# set :use_sudo, false
-# set :repo_url, "git@github.com:aayumalviya/cleco_store_api.git"
-# # Default branch is :master
-# #ask :branch, git rev-parse --abbrev-ref HEAD.chomp
-# set :branch, ENV['BRANCH'] || "main"
+set :application, 'clecostore'
+set :pty, true
+set :use_sudo, false
+set :repo_url, "git@github.com:aayumalviya/cleco_store_api.git"
+# Default branch is :master
+#ask :branch, git rev-parse --abbrev-ref HEAD.chomp
+set :branch, ENV['BRANCH'] || "main"
 
-# # Default deploy_to directory is /var/www/my_app_name
+# Default deploy_to directory is /var/www/my_app_name
 
-# set :user, "deploy"
+set :user, "deploy"
 
-# append :linked_dirs, '.bundle' # added after rails 6
+append :linked_dirs, '.bundle'    # added after rails 6
 
-# set :puma_threads,    [4, 16]
-# set :puma_workers,    0
-
-# # Don't change these unless you know what you're doing
-# set :production,      :production
-# set :deploy_via,      :remote_cache
-
-# set :rvm_type, :user                     # Defaults to: :auto
-# set :rvm_ruby_version, 'ruby-3.0.1'      # Defaults to: 'default'
-
-# set :deploy_to, "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
-# set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-# set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
-# set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
-# set :puma_access_log, "#{release_path}/log/puma.access.log"
-# set :puma_error_log,  "#{release_path}/log/puma.error.log"
-# set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
-# set :puma_preload_app, true
-# set :puma_worker_timeout, nil
-# set :puma_init_active_record, true  # Change to true if using ActiveRecord
-
-# set :linked_files, %w{config/master.key}
-# set :linked_dirs, %w{log tmp/pids tmp/cache public/uploads}
-
-# # set :linked_files, %w{config/production.key}
-
-# set :keep_releases, 6
-# set :puma_enable_socket_service, true
-# # Default branch is :master
-# # ask :branch, git rev-parse --abbrev-ref HEAD.chomp
-
-# # Default deploy_to directory is /var/www/my_app_name
-# # set :deploy_to, "/var/www/my_app_name"
-
-# # namespace :deploy do
-# #
-# #   desc 'Restart application'
-# #   task :restart do
-# #     # restart via puma
-# #   end
-# #
-# #
-# # end
-
-# # You can configure the Airbrussh format using :format_options.
-# # These are the defaults.
-# # set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
-
-
-
-
-
-
-
-
-
-
-
-
-
-# Change these
-server '3.217.135.81', roles: [:web, :app, :db], primary: true
-
-set :repo_url,        'git@github.com:aayumalviya/cleco_store_api.git'
-set :application,     'clecostore'
-set :user,            'deploy'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
 
 # Don't change these unless you know what you're doing
-set :pty,             true
-set :use_sudo,        false
-set :stage,           :production
+set :production,      :production
 set :deploy_via,      :remote_cache
-set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+
+set :rvm_type, :user                     # Defaults to: :auto
+set :rvm_ruby_version, 'ruby-3.0.1'      # Defaults to: 'default'
+
+set :deploy_to, "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
-set :puma_access_log, "#{release_path}/log/puma.error.log"
-set :puma_error_log,  "#{release_path}/log/puma.access.log"
+set :puma_access_log, "#{release_path}/log/puma.access.log"
+set :puma_error_log,  "#{release_path}/log/puma.error.log"
 set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
-set :puma_init_active_record, false  # Change to true if using ActiveRecord
+set :puma_init_active_record, true  # Change to true if using ActiveRecord
 
-## Defaults:
-# set :scm,           :git
-set :branch,        :main
-# set :format,        :pretty
-# set :log_level,     :debug
-set :keep_releases, 5
+set :linked_files, %w{config/master.key}
+set :linked_dirs, %w{log tmp/pids tmp/cache public/uploads}
 
-## Linked Files & Directories (Default None):
-set :linked_files, %w{config/database.yml config/master.key}
-set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+# set :linked_files, %w{config/production.key}
 
-namespace :puma do
-  desc 'Create Directories for Puma Pids and Socket'
-  task :make_dirs do
-    on roles(:app) do
-      execute "mkdir #{shared_path}/tmp/sockets -p"
-      execute "mkdir #{shared_path}/tmp/pids -p"
-    end
-  end
+set :keep_releases, 6
+set :puma_enable_socket_service, true
+# Default branch is :master
+# ask :branch, git rev-parse --abbrev-ref HEAD.chomp
 
-  before :start, :make_dirs
-end
+# Default deploy_to directory is /var/www/my_app_name
+# set :deploy_to, "/var/www/my_app_name"
 
-namespace :deploy do
-  desc "Make sure local git is in sync with remote."
-  task :check_revision do
-    on roles(:app) do
-      unless `git rev-parse HEAD` == `git rev-parse origin/main`
-        puts "WARNING: HEAD is not the same as origin/main"
-        puts "Run `git push` to sync changes."
-        exit
-      end
-    end
-  end
+# namespace :deploy do
+#
+#   desc 'Restart application'
+#   task :restart do
+#     # restart via puma
+#   end
+#
+#
+# end
 
-  desc 'Initial Deploy'
-  task :initial do
-    on roles(:app) do
-      before 'deploy:restart', 'puma:start'
-      invoke 'deploy'
-    end
-  end
-
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      invoke 'puma:restart'
-    end
-  end
-
-  before :starting,     :check_revision
-  after  :finishing,    :compile_assets
-  after  :finishing,    :cleanup
-  after  :finishing,    :restart
-end
-
-# ps aux | grep puma    # Get puma pid
-# kill -s SIGUSR2 pid   # Restart puma
-# kill -s SIGTERM pid   # Stop puma
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
