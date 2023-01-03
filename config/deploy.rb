de01d77648ff8468960d0e4d969924051a3925e3
manish-shrivastava <er.manishshrivastava@gmail.com>
2023-01-03 12:25:28 +0530

    changes in deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,8 +16,8 @@ set :user, "deploy"
 
 append :linked_dirs, '.bundle' # added after rails 6
 
-# set :puma_threads,    [4, 16]
-# set :puma_workers,    0
+set :puma_threads,    [4, 16]
+set :puma_workers,    0
 
 # Don't change these unless you know what you're doing
 set :production,      :production
@@ -27,15 +27,15 @@ set :rvm_type, :user                     # Defaults to: :auto
 set :rvm_ruby_version, 'ruby-3.0.1'      # Defaults to: 'default'
 
 set :deploy_to, "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/puma.sock"
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
-set :puma_access_log, "#{release_path}/log/puma.error.log"
-set :puma_error_log,  "#{release_path}/log/puma.access.log"
+set :puma_access_log, "#{release_path}/log/puma.access.log"
+set :puma_error_log,  "#{release_path}/log/puma.error.log"
 set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
-set :puma_init_active_record, false  # Change to true if using ActiveRecord
+set :puma_init_active_record, true  # Change to true if using ActiveRecord
 
 set :linked_files, %w{config/master.key}
 set :linked_dirs, %w{log tmp/pids tmp/cache public/uploads}
