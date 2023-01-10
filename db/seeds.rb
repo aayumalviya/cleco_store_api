e4181f4a5dff0bc3070126fd78aae24a17673366
manish-shrivastava <er.manishshrivastava@gmail.com>
2023-01-10 16:12:10 +0530

    added image

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,11 @@
- AdminUser.create!(email: 'admin@example.com', password: 'password2', password_confirmation: 'password2')
+# AdminUser.create!(email: 'admin@example.com', password: 'password2', password_confirmation: 'password2')
 
 categories = ['mens', 'women', 'kids']
-
-puts 'Saving Categories'
-categories.each do |category|
-  puts "Category: #{category}"
-  category = Category.find_or_create_by(name: category)
-	#category.logo.attach(io: File.open("#{Rails.root.to_path}/public/#{category}_category.jpg"), filename: 'ethnicwear.jpg')
+categories.each do |cat|
+  category = Category.find_or_create_by(name: cat)
+	category.logo.attach(io: File.open("#{Rails.root.to_path}/public/#{cat}_category.jpg"), filename: 'ethnicwear.jpg')
 end
-puts 'Categories Saved'
-puts ''
+
 
 mens_sub_categories = ['t-shirt', 'kurta', 'Hoodies']
 women_sub_categories = ['saree', 'suits']
@@ -17,20 +13,20 @@ kids_sub_categories = ['jacket', 'blazzer']
 
 puts 'Saving mens subcategories'
 category = Category.find_by(name: 'mens')
-mens_sub_categories.each do |sub_category|
-  puts "Subcategory: #{sub_category}"
-  sub_category = category.sub_categories.find_or_create_by(name: sub_category)
-  #sub_category.logo.attach(io: File.open("#{Rails.root.to_path}/public/#{sub_category}_sub_category.jpg"), filename: 'ethnicwear.jpg')
+mens_sub_categories.each do |sub_cat|
+  puts "Subcategory: #{sub_cat}"
+  sub_category = category.sub_categories.find_or_create_by(name: sub_cat)
+  sub_category.logo.attach(io: File.open("#{Rails.root.to_path}/public/#{sub_cat}_sub_category.jpg"), filename: 'ethnicwear.jpg')
 end
 puts 'mens subcategories Saved'
 puts ''
 
 puts 'Saving women subcategories'
 category = Category.find_by(name: 'women')
-women_sub_categories.each do |sub_category|
-  puts "Subcategory: #{sub_category}"
-  sub_category = category.sub_categories.find_or_create_by(name: sub_category)
-    #sub_category.logo.attach(io: File.open("#{Rails.root.to_path}/public/#{sub_category}_sub_category.jpg"), filename: 'ethnicwear.jpg')
+women_sub_categories.each do |sub_cat|
+  puts "Subcategory: #{sub_cat}"
+  sub_category = category.sub_categories.find_or_create_by(name: sub_cat)
+  sub_category.logo.attach(io: File.open("#{Rails.root.to_path}/public/#{sub_cat}_sub_category.jpg"), filename: 'ethnicwear.jpg')
 end
 puts 'women subcategories saved'
 puts ''
@@ -38,10 +34,10 @@ puts ''
 
 puts 'Saving kids subcategories'
 category = Category.find_by(name: 'kids')
-kids_sub_categories.each do |sub_category|
-  puts "Subcategory: #{sub_category}"
-  sub_category = category.sub_categories.find_or_create_by(name: sub_category)
-    #sub_category.logo.attach(io: File.open("#{Rails.root.to_path}/public/#{sub_category}_sub_category.jpg"), filename: 'ethnicwear.jpg')
+kids_sub_categories.each do |sub_cat|
+  puts "Subcategory: #{sub_cat}"
+  sub_category = category.sub_categories.find_or_create_by(name: sub_cat)
+  sub_category.logo.attach(io: File.open("#{Rails.root.to_path}/public/#{sub_cat}_sub_category.jpg"), filename: 'ethnicwear.jpg')
 end
 puts 'kids subcategories saved'
 puts ''
@@ -83,7 +79,25 @@ products.each do |product|
   puts "title: #{product[:title]}"
   category = Category.find_by(name: product[:category])
   sub_category = SubCategory.find_by(name: product[:sub_category])
-  Product.find_or_create_by(title: product[:title], product_description: product[:product_description], price: product[:price], old_price: product[:old_price], exclusive_price: product[:exclusive_price], product_details: product[:product_details], category_id: category.id, is_published: product[:is_published], sub_category_id: sub_category.id, is_recommended: product[:is_recommended], quantity: product[:quantity])
+  prod = Product.find_or_create_by(title: product[:title], product_description: product[:product_description], price: product[:price], old_price: product[:old_price], exclusive_price: product[:exclusive_price], product_details: product[:product_details], category_id: category.id, is_published: product[:is_published], sub_category_id: sub_category.id, is_recommended: product[:is_recommended], quantity: product[:quantity])
+  prod.image.attach(io: File.open("#{Rails.root.to_path}/public/default_product.jpg"), filename: 'ethnicwear.jpg')
 end
 puts 'products saved'
 puts ''
+
+
+
+
+# categories = ['mens', 'women', 'kids']
+
+# puts 'Saving Categories'
+# categories.each do |category|
+#   debugger
+#   puts "Category: #{category}"
+#   category = Category.find_or_create_by(name: category)
+#   category.logo.attach(io: File.open("#{Rails.root.to_path}/public/#{category}_category.jpg"), filename: 'ethnicwear.jpg')
+# end
+# puts 'Categories Saved'
+# puts ''
+
+# product.logo.attach(io: File.open("#{Rails.root.to_path}/public/#{product[:title]}_product.jpg"), filename: 'ethnicwear.jpg')
